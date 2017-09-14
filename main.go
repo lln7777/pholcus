8c4fdec56962b59db7595e5b6970c0ded4e4d039
Ansel <ca@jiagouyun.com>
2017-09-14 14:54:25 +0800

    面向自己的开发

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	_ "pholcus_libs"
+
 	"github.com/henrylee2cn/pholcus/exec"
 	_ "github.com/henrylee2cn/pholcus_lib" // 此为公开维护的spider规则库
-	// _ "github.com/henrylee2cn/pholcus_lib_pte" // 同样你也可以自由添加自己的规则库
 )
 
 func main() {
